e9a1d744fbf77ff44d971f2ff19af5b1bc44c2e2
Jack <jack.a.fredericksen@gmail.com>
2018-11-11 21:48:14 -0500

    Gat

---
+++ src/components/header.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+export default props => <h1>{props.headerText}</h1>
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { Link } from 'gatsby'
+import Header from '../components/header'
+
+export default () => (
+  <div style={{ color: 'purple', fontSize: '72px' }}>
+    <Link to="/contact/">Contact</Link>
+    <h1>Hello Gatsby!</h1>
+    <p> What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+)
